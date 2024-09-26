c517fe031d3ca3bfa84579cf2d12c7f2651ac58b
daishuai <daishuai@kedacom.com>
2024-09-26 19:54:50 +0800

    1

--- src/main/java/com/javapub/flowable/myflowable/task/AssignTask.java
+++ src/main/java/com/javapub/flowable/myflowable/task/AssignTask.java
@@ -5,6 +5,8 @@ import org.flowable.engine.delegate.DelegateExecution;
 import org.flowable.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 /**
  * @author keda
  * @date 2024/9/26 18:41:14
@@ -18,5 +20,11 @@ public class AssignTask implements JavaDelegate {
         log.info("分配接警员");
         String incidentCode = delegateExecution.getVariable("incidentCode", String.class);
         Boolean autoAssign = delegateExecution.getVariable("autoAssign", Boolean.class);
+        if (Boolean.TRUE.equals(autoAssign)) {
+            delegateExecution.setVariable("dispatchers", List.of("zhangsan", "heqifeng"));
+            delegateExecution.setVariable("assignResult", "success");
+        } else {
+            delegateExecution.setVariable("assignResult", "failed");
+        }
     }
 }
